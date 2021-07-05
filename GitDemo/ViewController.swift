9f669bd2b539ddb30ced96f64f89a7b6c8c73a7f
Andrii Oliievskyi <andrew@MacBook-Pro.local>
2021-07-04 19:51:15 -0700

    First commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Andrii Oliievskyi on 2021-07-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Another print statement")
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
